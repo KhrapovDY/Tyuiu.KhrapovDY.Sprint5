039cf470b4e6dfa2e848b2fdeb846ab5cbe974b3
Daniil Khrapov <Tyuiu.KhrapovDY@outlook.com>
2024-11-20 18:19:32 +0500

    Task1

--- Tyuiu.KhrapovDY.Sprint5.Task1.V1.Test/DataServiceTest.cs
+++ Tyuiu.KhrapovDY.Sprint5.Task1.V1.Test/DataServiceTest.cs
@@ -1,17 +1,16 @@
-using System.IO;
-
-namespace Tyuiu.KhrapovDY.Sprint5.Task0.V11.Test
+namespace Tyuiu.KhrapovDY.Sprint5.Task1.V1.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void ValidSaveToFileTextData()
+        public void TestMethod1()
         {
-            string path = @"C:\Users\User\source\repos\Tyuiu.KhrapovDY.Sprint5\Tyuiu.KhrapovDY.Sprint5.Task0.V11\bin\Debug\net8.0\OutPutFileTask0.txt";
+            string path = $@"C:\Users\User\source\repos\Tyuiu.KhrapovDY.Sprint5\Tyuiu.KhrapovDY.Sprint5.Task1.V1\bin\Debug\net8.0\OutPutFileTask0.txt";
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
+
             bool wait = true;
             Assert.AreEqual(wait, fileExists);
         }
