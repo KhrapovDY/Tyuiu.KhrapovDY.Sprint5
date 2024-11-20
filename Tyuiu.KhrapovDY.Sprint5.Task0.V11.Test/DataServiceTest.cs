637754faace4f989c9c09a7d70157d2e76f4ab39
Daniil Khrapov <Tyuiu.KhrapovDY@outlook.com>
2024-11-20 15:26:15 +0500

    Task0

---
+++ Tyuiu.KhrapovDY.Sprint5.Task0.V11.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using System.IO;
+
+namespace Tyuiu.KhrapovDY.Sprint5.Task0.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = $@"C:\Users\User\source\repos\Tyuiu.KhrapovDY.Sprint5\Tyuiu.KhrapovDY.Sprint5.Task0.V11\bin\Debug\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
