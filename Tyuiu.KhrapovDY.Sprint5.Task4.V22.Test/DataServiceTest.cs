8006bbb50f545aa52a724f89d73ee133e9deea7f
Daniil Khrapov <Tyuiu.KhrapovDY@outlook.com>
2024-11-27 08:59:25 +0500

    Task4

---
+++ Tyuiu.KhrapovDY.Sprint5.Task4.V22.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using System.IO;
+
+namespace Tyuiu.KhrapovDY.Sprint5.Task4.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"Ñ:\DataSprint5\InPutDataFileTask4V22.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KhrapovDY.Sprint5.Task4.V22/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.KhrapovDY.Sprint5.Task4.V22.Lib;
+
+namespace Tyuiu.KhrapovDY.Sprint5.Task4.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Храпов Д. Ю. | СМАРТб-24-1";
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                         *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                           *");
+            Console.WriteLine("* Задание #4                                                                        *");
+            Console.WriteLine("* Вариант #22                                                                       *");
+            Console.WriteLine("* Выполнил: Храпов Даниил Юрьевич | СМАРТб-24-1                                     *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask4V0.txt (файл взять из архива согласно *");
+            Console.WriteLine("* вашему варианту. Создать папку в ручную С:\\DataSprint5\\ и скопировать в неё     *");
+            Console.WriteLine("* файл) в котором есть вещественное значение.Прочитать значение из файла и          *");
+            Console.WriteLine("* подставить вместо Х в формуле y = x^3 * sin x - 4x. Вычислить значение по формуле *");
+            Console.WriteLine("* (Полученное значение округлить до трёх знаков после запятой) и вернуть полученный *");
+            Console.WriteLine("* результат на консоль.                                                             *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+
+            string path = @"С:\DataSprint5\InPutDataFileTask4V22.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
